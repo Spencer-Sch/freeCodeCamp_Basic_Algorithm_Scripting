6120d5f5ebbb5919ae5b6e7616db6615f1a0869b
Spencer-Sch <spencer.schoeneman@gmail.com>
2021-02-03 21:42:09 -0600

    Update JS file
    adding my solutions for lessons 6 through 11

--- FCC_algorithm-scripting.js
+++ FCC_algorithm-scripting.js
@@ -107,3 +107,141 @@ function largestOfFour(arr) {
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
 
 // Last Commit
+// Lesson 6
+
+// My initial answer
+function confirmEnding(str, target) {
+  let endOfString = str.slice(-target.length);
+  if (endOfString === target) {
+    return true;
+  }
+  return false;
+}
+
+confirmEnding("Bastian", "n");
+
+// My answer edited down
+function confirmEnding(str, target) {
+  if (str.slice(-target.length) === target) {
+    return true;
+  }
+  return false;
+}
+
+confirmEnding("Bastian", "n");
+
+// My answer edited further
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target;
+  
+}
+
+confirmEnding("Bastian", "n");
+
+// Lesson 7
+
+// My answer
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return "";
+  } else {
+    return str.concat(repeatStringNumTimes(str, num-1));
+  }
+}
+
+repeatStringNumTimes("abc", 3);
+
+// Alternate recursive solution
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
+
+repeatStringNumTimes("abc", 3);
+
+// Lesson 8
+
+// My initial answer
+function truncateString(str, num) {
+  
+  if (num >= str.length) {
+    return str;
+  }
+  return str.slice(0, num).concat("...");
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// My answer edited to include ( ? : )
+
+function truncateString(str, num) {
+  return num >= str.length ? str : str.slice(0, num).concat("...");
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// Lesson 9
+
+function findElement(arr, func) {
+  for (let x of arr) {
+    if (func(x) === true) {
+      return x;
+    }
+  }
+}
+
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+
+// Lesson 10
+
+// First answer
+function booWho(bool) {
+  if (bool === true) {
+    return true;
+  } else if (bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+booWho(null);
+
+// Second answer
+function booWho(bool) {
+  return bool === true || bool === false ? true : false;
+}
+
+booWho(null);
+
+// Edited second anser
+function booWho(bool) {
+  return bool === true || bool === false;
+}
+
+booWho(null);
+
+// Lesson 11
+// My first answer.  Needs work.  Will edit.
+function titleCase(str) {
+  let finalStr = "";
+  let strArr = str.split(" ");
+  console.log(strArr);
+  for (let i=0; i<strArr.length; i++) {
+    if (i>0) {
+      finalStr += " ";
+    }
+    for (let j=0; j<strArr[i].length; j++) {
+      if (j === 0) {
+        let capLetter = strArr[i][j].toUpperCase();
+        finalStr += capLetter;
+      } else {
+        let nonCapLetter = strArr[i][j].toLowerCase();
+        finalStr += nonCapLetter;
+      }
+    }
+    
+  }
+  return finalStr;
+}
+
+titleCase("I'm a little tea pot");
